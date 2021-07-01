5a87cd89ce2ea00446cf435379809524d619422d
Ubuntu <ubuntu@ip-172-31-27-29.ap-south-1.compute.internal>
2021-07-01 06:02:00 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react";
+
+Amplify.configure(awsconfig);
+
+class App extends React.Component {
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> Welcome to the DevOps World Fellas!!!! </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
